51cfc8dcc2c94b6fd461fc81a16aa9b658b59e69
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-08 17:57:24 +0200

    Tipo in method call fixed

--- java/NachKaufabschluss.java
+++ java/NachKaufabschluss.java
@@ -1,6 +1,6 @@
 public class NachKaufabschluss {
 	public static void main(String[] args) {
-		System.out.printn("Vielen Dank für Ihren Einkauf!");
+		System.out.println("Vielen Dank für Ihren Einkauf!");
 		System.out.println("Vielleicht können wir Ihnen weitere Produkte geben?");
 	}
 }
