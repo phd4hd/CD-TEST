71c502f7c879d9ecec1fe915ced6f40dc389b88b
Dr. Peter Dillinger <104060003+phd4hsma@users.noreply.github.com>
2023-05-10 15:53:11 +0200

    Create NachKaufabschluss.java

---
+++ java/NachKaufabschluss.java
@@ -0,0 +1,6 @@
+public class NachKaufabschluss {
+	public static void main(String[] args) {
+		System.out.printl("Vielen Dank für Ihren Einkauf!");
+		System.out.println("Vielleicht können wir Ihnen weitere Prodkute geben?");
+	}
+}
